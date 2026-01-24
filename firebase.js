725d816a151ace134e72042982b9ccaa27ff132e
ANIMX <110913296+itssantoshnotava@users.noreply.github.com>
2026-01-25 00:44:41 +0530

    feat: Integrate Firebase for backend services
    Adds Firebase SDK and initializes the application with the necessary configuration. This enables future backend features such as data persistence and authentication.

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWev2yh8XuvTp-Tfw3lCPzaulHKVtdig4",
+  authDomain: "please-focus-gng.firebaseapp.com",
+  databaseURL: "https://please-focus-gng-default-rtdb.firebaseio.com",
+  projectId: "please-focus-gng",
+  storageBucket: "please-focus-gng.firebasestorage.app",
+  messagingSenderId: "649533529275",
+  appId: "1:649533529275:web:be4129252ac9fa7a655d9e"
+};
+
+const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
