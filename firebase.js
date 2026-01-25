c899baa3f6f7b240651590980c0070bbfecbeb32
ANIMX <110913296+itssantoshnotava@users.noreply.github.com>
2026-01-25 11:53:13 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,7 +4,7 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyBWev2yh8XuvTp-Tfw3lCPzaulHKVtdig4",
   authDomain: "please-focus-gng.firebaseapp.com",
-  databaseURL: "https://please-focus-gng-default-rtdb.firebaseio.com",
+  databaseURL: "https://please-focus-gng-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "please-focus-gng",
   storageBucket: "please-focus-gng.firebasestorage.app",
   messagingSenderId: "649533529275",
